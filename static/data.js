c2d8ff62d2fb78ca94761f48825cf899ed744500
jobex11 <joblawal33@gmail.com>
2024-04-12 02:30:13 +0100

    added solana connection

---
+++ static/data.js
@@ -0,0 +1,41 @@
+export const data = {
+  program: {
+    candyMachine: "9rY7xSHnmfvenkwGmweFFnEmADQXTULGDwgcczYMbjR7",
+    candyGuard: "",
+    candyMachineCreator: "CdmBLjRGi7RJSLDWRAmfcXEswwrXbTUry274Ko2n7F43",
+    collectionMint: "HZUu9VLcbUW8z3RwHgzcFYxiSpNgor6ahZLGM9MRy6Hi",
+  },
+  items: [
+    {
+      name: "SMB #10",
+      image_hash: "\u000f%�V�$���\u001d�4��\u0004��J2\u001d�>\u001f�n���QC'Y",
+      image_link:
+        "https://arweave.net/a7JskKgCHlSrR6-T5QuUh-UAs2_YHTxGohCPeLPJmLc",
+      metadata_hash: "�7eRSt2b�\u0001[�G�卪����\u0002�<�\u0019c\u0017kU3B",
+      metadata_link:
+        "https://arweave.net/8lriBze_6SQVS_GE9brnQiSnI6BAEKQ15saY1aa7iDE",
+      onChain: true,
+    },
+    {
+      name: "SMB #1001",
+      image_hash: "�a���\u0019�;\u0016oA-]\b%ꁒ7��4ⷽ�:��F",
+      image_link:
+        "https://arweave.net/VEpzyLENAMHBcUe4LxnF6y8HdfYMFHGrxh0hDhSEoeU",
+      metadata_hash:
+        "�\u001aT�\u001eY�\u0018T����(w��\u001c\u0011���L�� �R\u0001\u0016��",
+      metadata_link:
+        "https://arweave.net/3wv6YC3Hye-x47Gla7MbJjrr5rnJxX9yHaGl3TU3LC0",
+      onChain: true,
+    },
+    {
+      name: "SMB #1002",
+      image_hash: "3v\u001a&]��3��\u0001�)��X�c%_\u001fTB�)U\u0011�YY",
+      image_link:
+        "https://arweave.net/rhm9fNhVetfqYw2Ww8NSa_40bQbMbtqbIwfomMOO1jY",
+      metadata_hash: "�.d����9$H\\��\u000e3�@����\u0002#�\u001a�M�� ��",
+      metadata_link:
+        "https://arweave.net/e9cDY0glnWmjnIwPn0FqjBpDyJuLcqDl16ggOqqVlmk",
+      onChain: true,
+    },
+  ],
+};
---
+++ utils/constants.js
@@ -0,0 +1,9 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const RPC_ENDPOINT =
+  "https://billowing-clean-dinghy.solana-devnet.quiknode.pro/34fdaded3e0e47c47609edf24c35535686caa53b/";
+
+// Change this with your candy machine id
+export const CANDY_MACHINE_ID = new PublicKey(
+  "4XiK3EM3gqTZn5r8ejYJ7df5Pbx75bdWPfCoSGJTDda5"
+);
